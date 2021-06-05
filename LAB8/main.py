9c2481dd4af2133a40622176e0581e524f8eec29
Osmany <osmanypr2013@gmail.com>
2021-06-05 01:09:51 -0400

    plot results

--- LAB8/main.py
+++ LAB8/main.py
@@ -1,25 +1,41 @@
 from viajante import viajante_ants
 from MIP_CBC import solve_CBC, generate_distance_matrix
 import matplotlib.pyplot as plt
+import numpy as np
 
 def main():
 
+    ants_times =[]
     ants_results =[]
-    mip_result =[]
+
+    mip_times =[]
+    mip_results =[]
+    x_axes= [i for i in range(2,15)]
     
 
-    for i in range(2,20):
+    for i in range(2,15):
         distances = generate_distance_matrix(i)
-        path, solver, colony = viajante_ants(distances)
+        path, solver, colony = viajante_ants(np.array(distances))
 
         duration_ants = solver.plugins['Timer'].duration
-        ants_results.append((i,duration_ants))
+        ants_times.append(duration_ants)
+        ants_results.append(path.cost)
 
         result, time =solve_CBC(i,distances)
-        mip_result.append((i,time))
+        mip_times.append(time)
+        mip_results.append(result)
     
-    plt.plot(ants_results,label="Ants")
-    plt.plot(mip_result, label= "CBC")
+    plt.plot(x_axes, ants_times,label="Ants")
+    plt.xlabel("Number of cities.")
+    plt.ylabel("Time taken to solve (seconds).")
+    plt.plot(x_axes ,mip_times, label= "CBC")
+    plt.legend()
+    plt.show()
+
+    plt.plot(x_axes, ants_results,label="Ants")
+    plt.xlabel("Number of cities.")
+    plt.ylabel("Costs.")
+    plt.plot(x_axes ,mip_results, label= "CBC")
     plt.legend()
     plt.show()
 
